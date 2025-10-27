e00ce49b942e277a4c0b3d9e3fc4e390eb61eec0
Diego Noriega <Diego.ale036@gmail.com>
2025-10-27 00:28:11 -0500

    chore: add icon assets for GitHub, Instagram, and theme variations 🎨

---
+++ src/assets/icons.js
@@ -0,0 +1,12 @@
+export const icon = {
+	githubLight: "src/assets/icons/github-light.png",
+	githubDark: "src/assets/icons/github-dark.png",
+	inLight: "src/assets/icons/in-light.png",
+	inDark: "src/assets/icons/in-dark.png",
+	instaLight: "src/assets/icons/insta-light.png",
+	instaDark: "src/assets/icons/insta-dark.png",
+	themeLight: "src/assets/icons/theme-Light.png",
+	themeDark: "src/assets/icons/themes-dark.png",
+	arrowLight: "src/assets/icons/arrow-light.png",
+	arrowDark: "src/assets/icons/arrow-dark.png",
+};
