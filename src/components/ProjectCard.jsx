2d718a9c88bf77597bcc2af4018f9b1c863a9663
Diego Noriega <Diego.ale036@gmail.com>
2025-10-28 20:10:18 -0500

    chore: refactor ProjectCard component for improved layout and content display 🎨

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,13 +1,30 @@
 
-function ProjectCard({ lang, cardInfo }) {
+function ProjectCard({ cardInfo }) {  
+
   return(
-    <article className="h-44 w-md rounded-xl dark:bg-gray-400 bg-grey-800">
-      <h3>Project Name</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, aliquid.</p>
-      <div>
-        <span>html</span>
-        <span>css</span>
-        <span>javascript</span>
+    <article className="flex flex-col justify-between resize-card-box border-2 dark:bg-grey-800 bg-grey-300 dark:border-grey-700 border-grey-400 dark:hover:border-grey-600 hover:border-grey-500 transition-colors duration-100 rounded-md p-4">
+      <h3 className="text-lg font-bold dark:text-grey-400 text-grey-800">{cardInfo.name}</h3>
+      <p className="text-balance font-medium resize-card-desc dark:text-grey-400 text-grey-700">{cardInfo.desc}</p>
+      <div className="flex gap-2 text-sm">
+        {cardInfo.tech.map((p, i) => <span key={i} className="float-desc-card">{p}</span>)}
+      </div>
+      <div className="flex gap-4 dark:text-grey-500 text-grey-800">
+        <a 
+        href={cardInfo.github}
+        aria-label="Link to the project in github"
+        className="card-links"
+        target="blank"
+        >
+        Github
+        </a>
+        <a 
+        href={cardInfo.website}
+        aria-label="Link to the project website"
+        className="card-links"
+        target="blank"
+        >
+        Website
+        </a>
       </div>
     </article>
   )
