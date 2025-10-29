db4ca70a8d164188e07a1d1dbe520005f62e85be
Diego Noriega <Diego.ale036@gmail.com>
2025-10-28 20:07:59 -0500

    chore: remove unused language state and related functionality from App component 🤓

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,12 @@ import { icon } from "./assets/icons"
 
 function App() {
   const [ theme, setTheme ] = useState(localStorage.getItem("theme") || "dark")
-  const [ lang, setLang ] = useState(localStorage.getItem("lang") || "EN")
 
   const changeTheme = () => {
     const newTheme = theme == "dark" ? "light" : "dark"
     setTheme(newTheme)
     localStorage.setItem("theme", newTheme)
   }
-
-  const changeLang = () => {
-    const newLang = lang == "EN" ? "ES" : "EN"
-    setLang(newLang)
-    localStorage.setItem("lang", newLang)
-  } 
 
   const github = theme == "dark" ? icon.githubLight : icon.githubDark;
   const linkedin = theme == "dark" ? icon.inLight : icon.inDark;
@@ -28,17 +21,13 @@ function App() {
   
 
   return (
-    <div className="grid grid-rows-[auto_auto_1fr_auto_auto] pl-10 pr-10 pt-4 pb-4 h-dvh bg-grey-100 dark:bg-grey-900 color-transition overflow-y-scroll" data-theme={theme}>
+    <div className="grid grid-rows-[auto_auto_1fr_auto_auto] grid-cols-1 pl-10 pr-10 pt-4 pb-4 h-dvh scroll-smooth bg-grey-100 dark:bg-grey-900 color-transition overflow-y-scroll" data-theme={theme}>
       <Header 
-      lang={lang} 
       onThemeChange={changeTheme} 
-      onLangChange={changeLang} 
       icon={{ github, arrow, themeIcon}} 
       />
       <div className="divider"></div>
       <Main
-      lang={lang} 
-      theme={theme} 
       icon={{ github, linkedin, insta }} 
       />
       <div className="divider"></div>
