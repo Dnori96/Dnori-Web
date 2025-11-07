efc745bd242962dcb041952634965ff62ad61c57
Diego Noriega <Diego.ale036@gmail.com>
2025-11-07 13:07:24 -0600

    feat: add Vercel Analytics integration and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Main from "./components/Main";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { icon } from "./assets/icons";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
 	const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
@@ -20,15 +21,13 @@ function App() {
 	const arrow = theme == "dark" ? icon.arrowLight : icon.arrowDark;
 
 	return (
-		<div
-			className="grid grid-rows-[auto_auto_1fr_auto_auto] grid-cols-1 pl-10 pr-10 pt-4 pb-4 h-dvh scroll-smooth bg-grey-100 dark:bg-grey-900 color-transition overflow-y-scroll"
-			data-theme={theme}
-		>
+		<div className="grid grid-rows-[auto_auto_1fr_auto_auto] grid-cols-1 pl-10 pr-10 pt-4 pb-4 h-dvh scroll-smooth bg-grey-100 dark:bg-grey-900 color-transition overflow-y-scroll" data-theme={theme}>
 			<Header onThemeChange={changeTheme} icon={{ github, arrow, themeIcon }} />
 			<div className="divider"></div>
 			<Main icon={{ github, linkedin, insta }} />
 			<div className="divider"></div>
 			<Footer onThemeChange={changeTheme} icon={{ themeIcon, github }} />
+			<Analytics />
 		</div>
 	);
 }
